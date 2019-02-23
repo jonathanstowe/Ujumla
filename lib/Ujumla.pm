5d6ce2ff2b2cd4d6efe410fb9c28b4bd7f1f862e
Jonathan Stowe <jns@gellyfish.co.uk>
2019-02-23 22:16:28 +0000

    Some stuff

--- lib/Ujumla.pm
+++ lib/Ujumla.pm
@@ -1,7 +1,22 @@
 use v6;
 
 class Ujumla {
-
-
+    grammar Grammar {
+        rule  TOP {
+            [ <config-line> | <comment> ]+
+        }
+        token name { \w+ }
+        token quote { <['"]> }
+        token value { \N* }
+        rule comment { ^^ \s* '#' .*?   $$ }
+        rule config-line {
+            <name><.separator><value>
+        }
+        token separator {
+            <space>|<equals>
+        }
+        token space { \s+ }
+        token equals { \s* '=' \s* }
+    }
 }
 # vim: expandtab shiftwidth=4 ft=perl6
